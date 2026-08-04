55e952383e943b0c3af380bee9e1769acdf36712
KiaraGeuten <kiara.geuten@student.ehb.be>
2026-08-04 19:53:46 +0200

    cards get route added

--- routes/cards.js
+++ routes/cards.js
@@ -3,17 +3,14 @@ import { getDB } from "../database.js";
 
 const router = express.Router();
 
+//get all cards
 router.get("/", async (req, res) => {
   try {
     const db = getDB();
-
     const cards = await db.collection("cards").find({}).toArray();
-
     res.send(cards);
   } catch (error) {
-    res.status(500).send({
-      error: error.message,
-    });
+    res.status(500).json({ message: error.message });
   }
 });
 
